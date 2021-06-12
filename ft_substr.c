005c288c426741c4d0c81515c118c5fbe3c60bdc
joaovgr <joaovictorgrodrigues@gmail.com>
2021-06-12 11:11:56 -0300

    substr protect

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jvictor- <jvictor-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/10 20:51:25 by jvictor-          #+#    #+#             */
-/*   Updated: 2021/06/12 10:44:32 by jvictor-         ###   ########.fr       */
+/*   Updated: 2021/06/12 11:10:14 by jvictor-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	i = 0;
 	s_len = ft_strlen((char *)s);
 	substr = (char *)malloc(len + 1);
-	if (substr == 0)
+	if (!substr)
 		return (0);
+	if (!s)
+		return(0);
 	if (s_len <= start)
 		return (substr);
 	while (s[start] && len > i)
