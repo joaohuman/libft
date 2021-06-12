a6151b4d83ed6ce3f4392ad9eca65bb134958f98
joaovgr <joaovictorgrodrigues@gmail.com>
2021-06-11 22:46:17 -0300

    memcpy with null

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: jvictor- <jvictor-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/02 19:56:42 by jvictor-          #+#    #+#             */
-/*   Updated: 2021/06/04 20:10:07 by jvictor-         ###   ########.fr       */
+/*   Updated: 2021/06/11 22:45:11 by jvictor-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	dest_cast = (char *)dest;
 	src_a = (char *)src;
 	i = 0;
+	if (!(dest_cast) || !(src_a))
+		return(0);
 	while (n--)
 	{
 		dest_cast[i] = src_a[i];
