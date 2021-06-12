5a0f9ac2ca880cd0a2a68da46e19a3670ca04256
joaovgr <joaovictorgrodrigues@gmail.com>
2021-06-12 11:01:34 -0300

    strjoin protected

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: jvictor- <jvictor-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/10 21:18:58 by jvictor-          #+#    #+#             */
-/*   Updated: 2021/06/12 10:54:42 by jvictor-         ###   ########.fr       */
+/*   Updated: 2021/06/12 11:01:02 by jvictor-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	j = 0;
 	concat = (char *)malloc(sizeof(s1) + sizeof(s2) + 1);
 	if (!concat)
+		return (0);
+	if (!s1 || !s2)
 		return (0);
 	while (s1[i])
 	{
