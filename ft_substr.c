7248b963cdfde123821198748f333886d2847c36
joaovgr <joaovictorgrodrigues@gmail.com>
2021-06-12 13:46:16 -0300

    substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,11 +6,12 @@
 /*   By: jvictor- <jvictor-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/10 20:51:25 by jvictor-          #+#    #+#             */
-/*   Updated: 2021/06/12 13:05:03 by jvictor-         ###   ########.fr       */
+/*   Updated: 2021/06/12 13:45:12 by jvictor-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -22,7 +23,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (0);
 	i = 0;
 	s_len = ft_strlen(s);
-	if (s_len < start)
+	if (s_len <= start)
 	{
 		substr = (char *)ft_calloc(sizeof(char), 1);
 		if (!substr)
